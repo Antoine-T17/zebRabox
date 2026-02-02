29e7ab7ec4f11ae386f7a8715fa1e73ac756695d
Antoine TOURRET <antoinetourretpro@gmail.com>
2026-02-02 19:24:58 +0100

    Commit 2
    run app, fixed launch.browser error

--- R/run_app.R
+++ R/run_app.R
@@ -1,7 +1,9 @@
 #' Run ZebRabox
 #' @export
-run_app <- function(...) {
+run_app <- function(..., launch.browser = TRUE) {
   app_dir <- system.file("app", package = "zebRabox")
   if (app_dir == "") stop("App directory not found. Is 'zebRabox' installed?")
+
   withr::with_dir(app_dir, shiny::runApp(app_dir, launch.browser = launch.browser, ...))
 }
+
