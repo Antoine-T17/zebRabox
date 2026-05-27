369619f4011d32486d30d8bc10eab158fecd80f9
Antoine TOURRET <antoinetourretpro@gmail.com>
2026-05-27 18:50:30 +0200

    Commit 20
    attack package namespace in run_app() so Shiny scripts can find exported functions

--- R/run_app.R
+++ R/run_app.R
@@ -4,6 +4,12 @@ run_app <- function(..., launch.browser = TRUE) {
   app_dir <- system.file("app", package = "zebRabox")
   if (app_dir == "") stop("App directory not found. Is 'zebRabox' installed?")
 
+  # When called via zebRabox::run_app(), the namespace is loaded but not
+  # attached to the search path. Shiny sources inst/app/ scripts into
+  # .GlobalEnv, so package functions (get_processing_config, etc.) would
+  # not be found without this explicit attach.
+  if (!"package:zebRabox" %in% search()) attachNamespace("zebRabox")
+
   tryCatch(
     withr::with_dir(app_dir, shiny::runApp(app_dir, launch.browser = launch.browser, ...)),
     interrupt = function(e) invisible(NULL)
